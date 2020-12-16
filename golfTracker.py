eb358409c6b8db1a91637d83db0456e0d3151a09
Stephen Lorenz <slorenz@bellarmine.edu>
2020-12-15 23:26:58 -0500

    full code (hopefully) with file writer

--- golfTracker.py
+++ golfTracker.py
@@ -2,9 +2,10 @@
 Created for Code Lou's end of course Python project
 ---info---
 This program tracks the golf score inputs of 2 players,
-then returns the name of the winner and their score,
-then asks if they want to play again
-the info is then saved to a text file with a name chosen by the user'''
+makes a temporary .txt file, uses regex to read from that file,
+then uses the read values to calculate that game's winner and
+displays them to the user, along withe the final scores
+'''
 
 
 p1 = input("Please enter player 1's name: ").split() #split turns p1 into list and ensures p1[0] is the whole input
@@ -16,16 +17,16 @@ while True:
     if answer.lower() == 'done': #game-ending condition
         break
     else:
-        p1.append(int(answer))
+        p1.append((answer))
 
-    p2.append(int(input('How many strokes did {} make on hole {}? '.format(p2[0], hole_num))))
+    p2.append(input('How many strokes did {} make on hole {}? '.format(p2[0], hole_num)))
     print()
     hole_num += 1
 
 #creates temporary .txt file for later regex to read
-with open (file_name, "w") as scoreCard:
-    for item in player1:
-        scoreCard.write(item,' ')
+with open ('golfGame.txt', "w") as scoreCard:
+    for item in p1:
+        scoreCard.write(item)
     scoreCard.write('\n')
-    for item in player2:
-        scoreCard.write(item,' ')
+    for item in p2:
+        scoreCard.write(item)
