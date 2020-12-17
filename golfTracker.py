85eced87c3540c2c03d107437313f65a477e89d6
Stephen Lorenz <slorenz@bellarmine.edu>
2020-12-17 17:29:43 -0500

    Final Update! - added more exception handling

--- golfTracker.py
+++ golfTracker.py
@@ -4,7 +4,9 @@ Created for Code Lou's end of course Python project
 This program tracks the golf score inputs of 2 players,
 makes a temporary .txt file, uses regex to read from that file,
 then uses the read values to calculate that game's winner and
-displays them to the user, along with the final scores
+displays them to the user, along with the final scores. I
+recognize adding the file-reading is inefficient and it's here
+purely for regex practice.
 '''
 import re
 import io
@@ -22,36 +24,44 @@ def write_temp(p1, p2): # creates golfGame.txt for later regex to read
 def read_grab_names(): # returns player names from golfGame.txt
     with open(temp_file_name, "r") as f:
         content1 = f.readline()
-        player1 = re.findall(r'^[A-z]+', content1)
+        player1 = re.findall(r'[-.A-z0-9 ]+ ', content1)
 
         content2 = f.readline()
-        player2 = re.findall(r'^[A-z]+', content2)
-        # ^[A-z] keeps accidental words/mistypes from replacing player names
-        # say golfGame.txt had Angus123donee on line 1 and Beef450 on line 2,
-        # 'donee' won't be matched and Beef is still returned as player2[0]
+        player2 = re.findall(r'[-.A-z0-9 ]+ ', content2)
 
         return player1[0], player2[0]
 
 
 def read_grab_scores(): # returns list with strokes from each hole
+    p1_name, p2_name = read_grab_names()
+    p1_score = 0
+    p2_score = 0
+
+# ------ p1_score ------
     with open(temp_file_name, "r") as f:
         contents = f.read()
-        golf_scores = re.findall(r'[0-9]+', contents)
+        golf_scores = re.findall(r' [0-9]+', contents)
+        # starting at ' ' keeps weird names with numbers, like BB-8, from being read
 
-        p1_score = 0
-        for num in golf_scores[0]: # if findall = [('1','2','3'), ('4','5','6')]
-            p1_score += int(num) # num is 1 2 3
+        for i in range(1, len(golf_scores[0])): # if findall gives [(' ','1','2','3'), (' ','4','5','6')]
+            p1_score += int(golf_scores[0][i]) # skip ' ', add 1 2 3
 
-        p2_score = 0
-        for num in golf_scores[1]:
-            p2_score += int(num) # num is 4 5 6
+# ------ p2_score ------
+    with open(temp_file_name, "r") as f:
+        contents = f.read()
+        golf_scores = re.findall(r' [0-9]+', contents)
 
-        return p1_score, p2_score
+        for i in range(1, len(golf_scores[1])): # if findall gives [(' ','1','2','3'), (' ','4','5','6')]
+            p2_score += int(golf_scores[1][i]) # skip ' ', add 4 5 6
+
+    return p1_score, p2_score
 
 
 # ------ user input section ------
 p1 = input("Please enter player 1's name: ").split() #split turns p1 into list and ensures p1[0] is the whole input
 p2 = input("Please enter the second player's name: ").split()
+p1.append(' ') # these spaces will tell read_grab_names where to stop reading
+p2.append(' ')
 
 hole_num = 1
 answer = ''
@@ -68,15 +78,15 @@ while True:
         else:
             p1.append(answer)
 
-    try:
-        answer = input('How many strokes did {} make on hole {}? '.format(p2[0], hole_num))
-        int(answer)        
-    except ValueError:
-        print("Oops, that's not a number, please try again and ENTER 0 for {}'s next value!\n".format(p1[0]))
-        continue # Yes, I'm relying on the user to read the instructions,
-                 # I don't know how to skip p1's section to get back to here
-    else:
-        p2.append(answer)
+    while True:
+        try:
+            answer = input('How many strokes did {} make on hole {}? '.format(p2[0], hole_num))
+            int(answer)        
+        except ValueError:
+            print("Oops, that's not a number, please try again!")
+        else:
+            p2.append(answer)
+            break
 
     print() #\n after the hole is done for readability
     hole_num += 1
@@ -86,8 +96,8 @@ p1_name, p2_name = read_grab_names()
 p1_score, p2_score = read_grab_scores()
 
 if p1_score < p2_score:
-    print('And the winner is {} with {} strokes made!'.format(p1_name, p1_score))
+    print('And the winner is {}with {} strokes made!'.format(p1_name, p1_score))
 elif p1_score == p2_score:
-    print('Woah, its a tie! {} and {} both made {} strokes!'.format(p1_name, p2_name, p1_score))
+    print('Woah, its a tie! {}and {}both made {} strokes!'.format(p1_name, p2_name, p1_score))
 else:
-    print('And the winner is {} with {} strokes made!'.format(p2_name, p2_score))
+    print('And the winner is {}with {} strokes made!'.format(p2_name, p2_score))
